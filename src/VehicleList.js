f240d6f755db0ba52b7cb5835218de701e5f7c27
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2022-01-26 16:58:10 -0800

    started traffic buttons, working properly

--- src/VehicleList.js
+++ src/VehicleList.js
@@ -5,7 +5,7 @@ export default function VehicleList(props) {
   return (
     <div className="traffic">
       {/* this component takes in a prop called vehicles, which is an array of vehicle strings */}
-      {props.vehicleArr.map((vehicle, i) =>
+      {props.traffic.map((vehicle, i) =>
         <Vehicle key={vehicle + i} vehicle={vehicle} />)}
       {/* map through the vehicles array in props */}
       {/* for each item render a Vehicle component. Pass the vehicl string as a prop called vehicle to the Vehicle component.  */}
