96548b9d8693c515324e684924adcf0a44b6b4d6
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-18 08:46:33 -0800

    starting test writing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Amazing! The alien zapped the lizard!/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders component traffic light', () => {
+  render(<App />); //do i need to import trafficeLights.js?
+
+
+  const linkElement = screen.getByText(/green/i);
+
+
+  expect(linkElement).toBeInTheDocument(); //setLightColor to 'green', button is active
 });
